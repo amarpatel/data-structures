9a9ad24fe1b72eed85f4c50dd4d52bc575b1428d
Amar Patel <theamarpatel@gmail.com>
2014-04-16 13:16:12 -0700

    Added additional comments

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -7,14 +7,24 @@ var makeStack = function(){
 
   // Implement the methods below
   instance.push = function(value){
+    //adds value to last index
     storage[size] = value;
+
+    //updates instance.size
     size++;
   };
 
   instance.pop = function(){
+    //decrements size as long as size >= 1
     size && size--;
+
+    //temp container for soon-to-be-returned value
     var result = storage[size];
+
+    //clears last index
     delete storage[stack];
+
+    //returns temp container
     return result;
   };
 
