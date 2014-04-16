58dbbd83b57474ce2222d24cb2b4607932124392
Amar Patel <theamarpatel@gmail.com>
2014-04-16 13:19:04 -0700

    Pasted solution from previous step into this file.

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,5 +1,47 @@
 var makeQueue = function(){
-  // Hey! Copy your code from src/functional/queue.js and paste it here
+  var instance = {};
+
+  // Use an object with numeric keys to store values
+  var storage = {};
+  var currentKey = 0;
+  var adjustedStartingIndex = 0;
+  var currentSize = 0;
+
+  // Implement the methods below
+
+  instance.enqueue = function(value){
+    //sets index to inputted value
+    storage[currentKey] = value;
+
+    //so additional values can be enqueued
+    currentKey++;
+
+    //updates instance.size
+    currentSize++;
+  };
+
+  instance.dequeue = function(){
+    //updates instance.size
+    currentSize && currentSize--;
+
+    //container for returned result
+    var result = storage[adjustedStartingIndex];
+
+    //clears dequeued index
+    delete storage[adjustedStartingIndex];
+
+    //updates starting index
+    adjustedStartingIndex++;
+
+    return result;
+  };
+  instance.size = function(){
+    return currentSize;
+  };
+  return instance;
 };
+
+var queue = makeQueue();
+
 
 var queueMethods = {};
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,5 +1,40 @@
-var makeStack = function() {
-  // Hey! Copy your code from src/functional/stack.js and paste it here
+var makeStack = function(){
+  var instance = {};
+
+  // Use an object with numeric keys to store values
+  var storage = {};
+  var size = 0; // Hint: set an initial value here
+
+  // Implement the methods below
+  instance.push = function(value){
+    //adds value to last index
+    storage[size] = value;
+
+    //updates instance.size
+    size++;
+  };
+
+  instance.pop = function(){
+    //decrements size as long as size >= 1
+    size && size--;
+
+    //temp container for soon-to-be-returned value
+    var result = storage[size];
+
+    //clears last index
+    delete storage[stack];
+
+    //returns temp container
+    return result;
+  };
+
+  instance.size = function(){
+    return size;
+  };
+
+  return instance;
 };
+
+var stack = makeStack();
   
 var stackMethods = {};
