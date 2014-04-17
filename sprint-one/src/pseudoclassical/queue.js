1f9661b05e7b46d239a647d4e51f933d06e01f5d
Amar Patel <theamarpatel@gmail.com>
2014-04-16 19:32:29 -0700

    Completed all but one spec

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,3 +1,26 @@
 var Queue = function() {
-  // Hey! Copy your code from src/prototypal/queue.js and paste it here
+    storage = {};
+    currentKey = 0;
+    adjustedStartingIndex = 0;
+    currentSize = 0;
+};
+
+
+
+Queue.prototype.enqueue = function(value){
+  storage[currentKey] = value;
+  currentKey++;
+  currentSize++;
+};
+
+Queue.prototype.dequeue = function(){
+  currentSize && currentSize--;
+  var result = storage[adjustedStartingIndex];
+  delete storage[adjustedStartingIndex];
+  adjustedStartingIndex++;
+  return result;
+};
+
+Queue.prototype.size = function(){
+  return currentSize;
 };
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,3 +1,25 @@
 var Stack = function() {
-  // Hey! Copy your code from src/prototypal/stack.js and paste it here
+  // Use an object with numeric keys to store values
+  storage = {};
+  size = 0;
 };
+
+  
+
+Stack.prototype.push = function(value){
+  //adds value to last index
+  storage[size] = value;
+  size++;
+};
+
+Stack.prototype.pop = function(){
+  size && size--;
+  var result = storage[size];
+  delete storage[size];
+  return result;
+};
+
+Stack.prototype.size = function(){
+  return size;
+};
+
