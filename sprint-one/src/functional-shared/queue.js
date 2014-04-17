04f4142574d6ec2274c82fa08648ca6b52959cf9
Amar Patel <theamarpatel@gmail.com>
2014-04-16 17:25:50 -0700

    Passes all spec tests

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -2,46 +2,34 @@ var makeQueue = function(){
   var instance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
-  var currentKey = 0;
-  var adjustedStartingIndex = 0;
-  var currentSize = 0;
+    instance._storage = {};
+    instance._currentKey = 0;
+    instance._adjustedStartingIndex = 0;
+    instance._currentSize = 0;
 
+    _.extend(instance, queueMethods);
   // Implement the methods below
-
-  instance.enqueue = function(value){
-    //sets index to inputted value
-    storage[currentKey] = value;
-
-    //so additional values can be enqueued
-    currentKey++;
-
-    //updates instance.size
-    currentSize++;
-  };
-
-  instance.dequeue = function(){
-    //updates instance.size
-    currentSize && currentSize--;
-
-    //container for returned result
-    var result = storage[adjustedStartingIndex];
-
-    //clears dequeued index
-    delete storage[adjustedStartingIndex];
-
-    //updates starting index
-    adjustedStartingIndex++;
-
-    return result;
-  };
-  instance.size = function(){
-    return currentSize;
-  };
+  
   return instance;
 };
 
-var queue = makeQueue();
 
 
 var queueMethods = {};
+queueMethods.enqueue = function(value){
+  this._storage[this._currentKey] = value;
+  this._currentKey++;
+  this._currentSize++;
+};
+
+queueMethods.dequeue = function(){
+  this._currentSize && this._currentSize--;
+  var result = this._storage[this._adjustedStartingIndex];
+  delete this._storage[this._adjustedStartingIndex];
+  this._adjustedStartingIndex++;
+  return result;
+};
+
+queueMethods.size = function(){
+  return this._currentSize;
+};
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,40 +1,28 @@
 var makeStack = function(){
   var instance = {};
-
   // Use an object with numeric keys to store values
-  var storage = {};
-  var size = 0; // Hint: set an initial value here
-
-  // Implement the methods below
-  instance.push = function(value){
-    //adds value to last index
-    storage[size] = value;
-
-    //updates instance.size
-    size++;
-  };
-
-  instance.pop = function(){
-    //decrements size as long as size >= 1
-    size && size--;
-
-    //temp container for soon-to-be-returned value
-    var result = storage[size];
-
-    //clears last index
-    delete storage[stack];
-
-    //returns temp container
-    return result;
-  };
-
-  instance.size = function(){
-    return size;
-  };
-
+  instance._storage = {};
+  instance._size = 0; // Hint: set an initial value here
+  _.extend(instance, stackMethods);
   return instance;
 };
 
-var stack = makeStack();
   
 var stackMethods = {};
+
+stackMethods.push = function(value){
+  //adds value to last index
+  this._storage[this._size] = value;
+  this._size++;
+};
+
+stackMethods.pop = function(){
+  this._size && this._size--;
+  var result = this._storage[this._size];
+  delete this._storage[this._size];
+  return result;
+};
+
+stackMethods.size = function(){
+  return this._size;
+};
