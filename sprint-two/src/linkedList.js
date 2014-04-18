9682baaf259bef26ff2035f35a9dae87fc83848c
Amar Patel <theamarpatel@gmail.com>
2014-04-17 17:14:31 -0700

    Attempt 1

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,7 +3,34 @@ var makeLinkedList = function(){
   list.head = null;
   list.tail = null;
 
+  /*
+
+  list = {
+
+
+    head: null,
+    tail: null;
+  };
+
+
+  makeNode(4)
+    //{value: 4, next: null}
+
+
+  list = {
+
+
+    head: null,
+    tail: null;
+
+  };
+
+
+  */
+
   list.addToTail = function(value){
+    this.value = makeNode(value);
+    this.tail = this.value;
   };
 
   list.removeHead = function(){
