7d05036ab6c456c4749dc19bb966ba4931e99c94
Amar Patel <theamarpatel@gmail.com>
2014-04-18 11:05:31 -0700

    Completed all specs

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,7 +1,7 @@
 var makeTree = function(value){
-  var newTree = {};
+  var newTree = Object.create(treeMethods);
   newTree.value = value;
-  newTree.children = undefined;
+  newTree.children = [];
   return newTree;
 };
 
@@ -9,8 +9,23 @@ var makeTree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
+  var treeInstance = makeTree(value);
+  this.children.push(treeInstance);
 };
 
 treeMethods.contains = function(target){
-};
+  if (this.value === target) {
+    return true;
+  } else {
+    //recursive case
+    for (var i=0;i<this.children.length;i++) {
+      if(this.children[i]) {
+        if (this.children[i].contains(target)) {
+          return true;
+        }
+      }
+    }
+  }
 
+  return false;
+};
