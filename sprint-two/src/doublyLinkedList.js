8fc809e6028c58519b8057b776a9bcbe38123114
Amar Patel <theamarpatel@gmail.com>
2014-04-20 01:21:11 -0700

    Completed all specs

--- sprint-two/src/doublyLinkedList.js
+++ sprint-two/src/doublyLinkedList.js
@@ -28,8 +28,50 @@ var makeDoublyLinkedList = function(){
     }
   };
 
+  list.addToHead = function (value) {
+    var tempObj;
+    if (!this.head && !this.tail) {
+      //add value
+      this.head = makeNode(value);
+    } else if (!this.tail) {
+      this.tail = this.head;
+      this.head = makeNode(value);
+      this.head.next = this.tail;
+      this.tail.previous = this.head;
+    } else {
+      //create a temp object
+      tempObj = {};
+      tempObj.value = this.head.value;
+      tempObj.next = this.head.next;
+      tempObj.next.previous = tempObj;
+
+      //create new head
+      this.head = makeNode(value);
+
+      //reassign head's next value
+      this.head.next = tempObj;
+
+      //assign old head's previous value to current head
+      tempObj.previous = this.head;
+    }
+  };
+
   list.removeHead = function(){
     this.head = this.head.next;
+  };
+
+  list.removeTail = function () {
+    var self = this;
+    var tailRemover = function (currentHead) {
+      currentHead = currentHead || self.head;
+      if (currentHead.next === self.tail) {
+        currentHead.next = null;
+        self.tail = currentHead;
+      } else {
+        tailRemover(currentHead.next);
+      }
+    };
+    tailRemover();
   };
 
   list.contains = function(target, node){
@@ -43,26 +85,6 @@ var makeDoublyLinkedList = function(){
       return false;
     }
   };
-
-  list.addToHead = function (value) {
-    if (!this.head && !this.tail) {
-      //add value
-      this.head = makeNode(value);
-    } else if (!this.tail) {
-      this.tail = this.head;
-      this.head = makeNode(value);
-      this.head.next = this.tail;
-      this.tail.previous = this.head;
-    } else {
-      //make nesting function!
-
-    }
-
-    //if head and tail are the same
-
-  };
-
-  list.removeTail = function () {};
 
   return list;
 };
